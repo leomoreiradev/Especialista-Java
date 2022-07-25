c34d163d124067b8254d95acc3214e50a95a1b8d
LEANDRO MOREIRA <leandrodev.oliveira@gmail.com>
2022-07-25 21:50:31 +0100

    Adicionando editorConfig

---
+++ PrimeiroProjeto/src/OlaMergulhador.java
@@ -0,0 +1,14 @@
+public class OlaMergulhador {
+    public static void main(String[] args) {
+
+        int num = 5;
+        String  nome = "leo";
+        boolean pendente = true;
+        System.out.printf("Teste: %d", num);
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("Numero: %d/10%n" ,i );
+        }
+
+    }
+}
